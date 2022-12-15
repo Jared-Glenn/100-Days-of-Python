e1b52ec1daf547d8d26f36602debdad36497e2dc
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-12-14 23:04:27 -0700

    Day 85
    Day 85 - Typing Speed Test
    * **Solo Project**
    * Created an app to test typing speed for the user. Roughly 200 characters are in each test (an amount that can be adjusted, as needed) and words are pulled from the the top 1000 most used English words.
    * A professional UI with clear markings for each button, with words and colors changing to demonstrate their uses or meanings.
    * Keeps track of if the user is correctly transcribing and gently corrects the user if a word has been written incorrectly.

---
+++ Days 76-100/085_typing_speed_test/typing_clocker.py
@@ -0,0 +1,33 @@
+from datetime import time
+import math
+
+
+class TypingClocker:
+    def __init__(self):
+        self.running = False
+        self.counter = 0
+
+    def start_timer(self):
+        self.counter = 0
+        self.running = True
+
+
+    def stop_timer(self):
+        self.running = False
+
+    def count(self):
+        if self.running:
+            seconds = self.counter % 60
+            minutes = (self.counter // 60) % 60
+
+            dt = time(second=seconds, minute=minutes)
+            string = dt.isoformat(timespec='auto')
+            self.counter += 1
+
+            return string
+
+    def calculate_wpm(self, words):
+        self.stop_timer()
+        words_per_second = words/self.counter
+        wpm = math.floor(words_per_second * 60)
+        return wpm

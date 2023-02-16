0b58355ce94e665b7fa284e2775e488d0d38d884
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2023-02-15 22:36:40 -0700

    Day 88
    Day 88
    * Completed a tutorial for database management.
    * Found and corrected the bug preventing database management.

--- Days 76-100/088_adjutant/sqlalchemy_tutorial/tutorial.py
+++ Days 76-100/088_adjutant/sqlalchemy_tutorial/tutorial.py
@@ -1,34 +1,130 @@
-import sqlalchemy as db
-from sqlalchemy import *
+from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, String, ForeignKey, select, insert
+from sqlalchemy.orm import Session, registry, relationship
 
-engine = db.create_engine("sqlite:///datacamp.sqlite")
-conn = engine.connect()
+engine = create_engine("sqlite+pysqlite:///tutorial.db", echo=True, future=True)
+metadata_obj = MetaData()
 
-metadata = db.MetaData()
+# with engine.connect() as conn:
+#     result=conn.execute(text("select 'hello world'"))
+#     print(result.all())
 
-Student = db.Table('Student', metadata,
-                   db.Column('Id', db.Integer(), primary_key=True),
-                   db.Column('Name', db.String(255), nullable=False),
-                   db.Column('Major', db.String(255), default='Math'),
-                   db.Column('Pass', db.Boolean(), default=True)
-                   )
+# # Commit as you go
+# with engine.connect() as conn:
+#     conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+#     conn.execute(
+#         text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+#         [{"x":1, "y":1}, {"x":2, "y":4}]
+#     )
+#     conn.commit()
 
-metadata.create_all(engine)
+# # Begin once
+# with engine.begin() as conn:
+#     conn.execute(
+#         text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+#         [{"x":6, "y":8}, {"x": 9, "y":10}],
+#     )
+    
+# with engine.connect() as conn:
+#     result = conn.execute(text("SELECT x, y FROM some_table"))
+#     for row in result:
+#         print(f'x: {row.x} y: {row.y}')
+        
+# with engine.connect() as conn:
+#     result = conn.execute(text("SELECT x, y FROM some_table WHERE y > :y"), {"y":2})
+#     for row in result:
+#         print(f"x: {row.x} y: {row.y}")
 
-for x in range(3):
-    print("New!")
 
-query = db.insert(Student).values(Id=1, Name='Matthew', Major='English', Pass=True)
-Result = conn.execute(query)
+# with engine.connect() as conn:
+#     conn.execute(
+#         text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+#         [{"x": 11, "y": 12}, {"x": 13, "y": 14}],
+#     )
+#     conn.commit()
+    
+# with engine.connect() as conn:
+#     result = conn.execute(text("SELECT x, y FROM some_table"))
+#     for row in result:
+#         print(f'x: {row.x} y: {row.y}')
 
-output = conn.execute(Student.select()).fetchall()
-print(output)
 
-query = db.insert(Student)
-values_list = [{'Id':'2', 'Name':'Nisha', 'Major':'Science', 'Pass':False},
-               {'Id':'3', 'Name':'Natasha', 'Major':'Math', 'Pass':True},
-               {'Id':'4', 'Name':'Ben', 'Major':'English', 'Pass':False}]
-Result = conn.execute(query, values_list)
+# stmt = text("SELECT x, y FROM some_table WHERE y > :y ORDER BY x, y")
+# with Session(engine) as session:
+#     result = session.execute(stmt, {"y": 6})
+#     for row in result:
+#         print(f"x: {row.x} y: {row.y}")
 
-output = conn.execute(db.select([Student])).fetchall()
-print(output)
+
+# with Session(engine) as session:
+#     result = session.execute(
+#         text("UPDATE some_table SET y=:y WHERE x=:x"),
+#         [{"x": 9, "y": 11}, {"x": 13, "y": 15}],
+#     )
+#     session.commit()
+
+
+
+
+# user_table = Table(
+#     "user_account",
+#     metadata_obj,
+#     Column("id", Integer, primary_key=True),
+#     Column("name", String(30)),
+#     Column("fullname", String),
+# )
+
+
+# print(user_table.c.name)
+
+# print(user_table.c.keys())
+
+# print(user_table.primary_key)
+
+# address_table = Table(
+#     "address",
+#     metadata_obj,
+#     Column("id", Integer, primary_key=True),
+#     Column("user_id", ForeignKey("user_account.id"), nullable=False),
+#     Column("email_address", String, nullable=False),
+# )
+
+# metadata_obj.create_all(engine)
+
+
+mapper_registry = registry()
+
+Base = mapper_registry.generate_base()
+
+user_table = Table(
+    "user_account",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(30)),
+    Column("fullname", String),
+)
+
+
+class User(Base):
+    __table__ = user_table
+    __table_args__ = {'extend_existing': True}
+    
+    def __repr__(self):
+        return f"User(name={self.name!r}, fullname={self.fullname!r})"
+    
+    
+sandy = User(name="sandy", fullname="Sandy Cheeks")
+
+metadata_obj.create_all(engine)
+
+stmt = insert(user_table).values(name="spongebob", fullname="Spongebob Squarepants")
+
+compiled = stmt.compile()
+
+
+with engine.connect() as conn:
+    result = conn.execute(stmt)
+    conn.commit()
+
+
+print(result.inserted_primary_key)
+

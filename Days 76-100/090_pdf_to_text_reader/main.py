d44ded926a9e6ea71dfffdf544786c2572c8a821
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2023-03-03 14:55:41 -0700

    Day 90
    Day 90 - PDF to Text Reader
    * **Solo Project**
    * Created an interface to select a PDF and Convert it.
    * Conversion makes use of api.voicerss.org to create a text to speech mp3.
    * Supports all PDFs.

--- Days 76-100/090_pdf_to_text_reader/main.py
+++ Days 76-100/090_pdf_to_text_reader/main.py
@@ -1,7 +1,81 @@
 import PyPDF2
+import requests
+from tkinter import *
+from tkinter import filedialog
+from security import API_KEY
 
-reader = PyPDF2.PdfReader('C:/Users/Jared/Downloads/Taming Survey.pdf')
 
-print(len(reader.pages))
+api_key = API_KEY
+text_str = ""
 
-print(reader.pages[0].extract_text())
+
+def get_text(location):
+    reader = PyPDF2.PdfReader(location)
+
+    length = (len(reader.pages))
+
+    text_str = ""
+    for page in range(length):
+        new_page = reader.pages[page].extract_text()
+        text_str += new_page
+    
+    return text_str
+
+
+def get_voice(text):
+    parameters = {
+        "key": api_key,
+        "hl": 'en-us',
+        "src": text,
+    }
+
+
+    response = requests.get(url="http://api.voicerss.org", params=parameters)
+    response.raise_for_status()
+
+    with open('speech.mp3', 'wb') as file:
+        file.write(response.content)
+
+
+def upload_action(event=None):
+    filename = filedialog.askopenfilename()
+    pdf_entry.delete(0, END)
+    pdf_entry.insert(0, filename)
+    
+    
+def convert(event=None):
+    location = pdf_entry.get()
+    print(location)
+    text = get_text(location)
+    get_voice(text)
+
+
+# ---------------------------------- TK INTER ----------------------------------------
+
+BACKGROUND_COLOR = "#2a82e9"
+TEXT_COLOR = "white"
+
+
+window = Tk()
+window.title('PDF Reader')
+window.config(pady=10, padx=10, bg=BACKGROUND_COLOR)
+
+title_label = Label(text='PDF Reader', fg=TEXT_COLOR, bg=BACKGROUND_COLOR, font=("Ariel", 20, "bold"))
+title_label.grid(column=0, row=0, columnspan=3)
+
+# PDF Upload
+file_label = Label(text="PDF File", fg=TEXT_COLOR, bg=BACKGROUND_COLOR, font=("Ariel", 10, "bold"))
+file_label.grid(column=0, row=1)
+
+pdf_entry = Entry(width=40)
+pdf_entry.grid(column=1, row=1, sticky=W)
+
+upload_button = Button(window, text="Select PDF", command=upload_action)
+upload_button.grid(column=2, row=1, padx=5)
+
+convert_button = Button(window, text="Convert", command=convert)
+convert_button.grid(column=1, row=2)
+
+
+window.mainloop()
+

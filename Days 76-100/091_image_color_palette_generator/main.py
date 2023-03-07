6a696fd1b79abfa3a977a8a9b5477c40d93b30e2
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2023-03-06 23:39:21 -0700

    Day 91
    Day 91
    * Made use of ColorThief to access the most used color in an image.
    * Found how to access palettes, as well.

--- Days 76-100/091_image_color_palette_generator/main.py
+++ Days 76-100/091_image_color_palette_generator/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 import os
 from werkzeug.utils import secure_filename
+from colorthief import ColorThief
 
 app = Flask(__name__)
 
@@ -20,12 +21,17 @@ def palette():
     if request.method=="POST":
         uploaded_img = request.files['uploaded-file']
         img_filename = secure_filename(uploaded_img.filename)
-        print("NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT NEXT ")
-        print(img_filename)
         uploaded_img.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
         img_file_path = UPLOAD_FOLDER + "/" + img_filename
+        updated_img_path = "/Image/" + img_filename
         
-        return render_template('palette_page.html', file=img_file_path)
+        color_thief = ColorThief(img_file_path)
+        dominant_color = color_thief.get_color(quality=1)
+        
+        #NEEDS TO BE IMPLEMENTED
+        palette = color_thief.get_palette(color_count=6)
+        
+        return render_template('palette_page.html', file=updated_img_path, dominant_color=dominant_color)
     
     else:
         return render_template(url_for(home))

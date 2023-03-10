21885440a926b15cca944df7b568ae4eba899f2e
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2023-03-09 23:14:54 -0700

    Day 91
    Day 91:
    * Worked on fixing bugs.

--- Days 76-100/091_image_color_palette_generator/main.py
+++ Days 76-100/091_image_color_palette_generator/main.py
@@ -29,7 +29,11 @@ def palette():
         dominant_color = color_thief.get_color(quality=1)
         
         #NEEDS TO BE IMPLEMENTED
-        palette = color_thief.get_palette(color_count=10)
+        first_palette = color_thief.get_palette(color_count=10)
+        palette = []
+        for color in first_palette:
+            color = "rgb" + str(color)
+            palette.append(color)
         
         return render_template('palette_page.html', file=updated_img_path, palette=palette)
     

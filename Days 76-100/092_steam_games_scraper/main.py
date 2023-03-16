11b29a210055fcfa3289a6adf03b5fcc9ecebe98
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2023-03-15 22:48:17 -0600

    Day 92
    Day 92
    * Conceptualized what needs to be scraped from the site to be useful.
    * Obtained the proper site address for the search.

--- Days 76-100/092_steam_games_scraper/main.py
+++ Days 76-100/092_steam_games_scraper/main.py
@@ -1,2 +1,6 @@
 from bs4 import BeautifulSoup
 
+# locate top rated games that are at matching lows
+# see about getting top reviews for each of these games
+
+# search address: https://steamdb.info/sales/?min_reviews=100&min_rating=95&min_discount=0

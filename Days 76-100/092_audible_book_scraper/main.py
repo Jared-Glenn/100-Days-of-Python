886fdb1320498a13a7cb91c30c4cd09c4b16576a
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2023-03-27 15:53:16 -0600

    Day 92
    Day 92 - Audible Book Scraper
    * **Solo Project**
    * Created a web scraper that can access Audible to locate new audio books.
    * The search terms can be altered by simply changing the link to match your desired search.
    * The scraper will provide a CSV of the titles, prices, number of stars rated, and the number of reviewers on the book.

--- Days 76-100/092_audible_book_scraper/main.py
+++ Days 76-100/092_audible_book_scraper/main.py
@@ -1,11 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-import time
+import csv
 
-# locate top rated games that are at matching lows
-# see about getting top reviews for each of these games
-
-# search address: https://steamdb.info/sales/?min_reviews=100&min_rating=95&min_discount=0
 
 site = "https://www.audible.com/search?creativeId=9648f6bf-4f29-4fb4-9489-33163c0bb63e&creativeId=9648f6bf-4f29-4fb4-9489-33163c0bb63e&creativeId=9648f6bf-4f29-4fb4-9489-33163c0bb63e&creativeId=9648f6bf-4f29-4fb4-9489-33163c0bb63e&feature_seven_browse-bin=18685633011&keywords=book&node=18580628011&pageLoadId=gcEEkgzwcLjacZkg&pageLoadId=Q6ROfiWTssPzMFv1&pageLoadId=SOa7pAbMYNmzYWgO&pageLoadId=Q7z6dlfobA00hxhX&publication_date=18685638011&ref=a_search_l1_feature_seven_browse-bin_3&pf_rd_p=daf0f1c8-2865-4989-87fb-15115ba5a6d2&pf_rd_r=55JCAYB0RQMN9WC9ZTZ2&pageLoadId=kQtjdB96bzEHRoEF&creativeId=9648f6bf-4f29-4fb4-9489-33163c0bb63e"
 
@@ -21,45 +17,55 @@ if '404' in response.url:
 
 # This will not print on its own without '.encode(utf-8)' but bs4 needs to parse it without the encode.
 soup = BeautifulSoup(response.content, "html.parser")
-spoonful = soup.find_all('div', attrs={"id": "product-list-a11y-skiplink-target"})
 
-table = spoonful.find_all('ul', attrs={"class": "bc-list"})
+book_lib = []
 
-for i in table:
-    print(i.text)
-    print('\n')
-
-# grid = soup.select_one["div{class=bc-col-responsive}"]
-# listings = grid.find_all("tr")
-# listing_list = []
-
-# print(grid)
-
-# /html/body/div[1]/div[5]/div[5]/div/div[2]
+# Add book title to list
+h3_list = soup.find_all("h3")
+for h3 in h3_list:
+    links = h3.find_all("a")
+    if links:
+        for link in links:
+            title = link.string
+            book_lib.append([title])
 
 
-# for listing in listings:
-#     if listing.select_one("a"):
-#         address = listing.select_one("a").text
-#         if listing.select_one("a").text == "Chevron Left":
-#             break
-#     else:
-#         address = "N/A"
-#     if listing.select_one("span"):
-#         price_start = listing.select_one("span").text
-#         price = price_start.split("+")[0]
-#     else:
-#         price = "N/A"
-#     if listing.select_one("a"):
-#         link = listing.select_one("a").get("href")
-#         link = "https://www.zillow.com/" + link
-#     else:
-#         link = "N/A"
-#     if address == "N/A" and price == "N/A" and link == "N/A":
-#         continue
-#     listing_list.append({
-#         "address": address,
-#         "price": price,
-#         "link": link,
-#     })
+# Create a price list
+price_list = []
+p_list = soup.find_all("p")
+for p in p_list:
+    spans = p.find_all("span")
+    if spans:
+        for span in spans:
+            price = span.string.strip()
+            if price.find("$") >= 0:
+                price_list.append(price)
+
+# Match prices to their books.
+index = 0
+for x, price in enumerate(price_list):
+    if x%2 == 0:
+        if book_lib[index]:
+            book_lib[index].append(price)
+            index += 1
+
+# Find ratings information (Note the underscore after class due to 'class' being reserved)
+index = 0
+li_list = soup.find_all(class_="ratingsLabel")
+for li in li_list:
+    spans = li.find_all(class_="bc-text bc-pub-offscreen")
+    for span in spans:
+        stars = span.string.strip()
+        book_lib[index].append(stars)
+    spans = li.find_all(class_="bc-text bc-size-small bc-color-secondary")
+    for span in spans:
+        ratings = span.string.strip()
+        book_lib[index].append(ratings)
+    index += 1
+
+
+# Move the list of lists into a CSV file.
+with open("output.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(book_lib)
 
